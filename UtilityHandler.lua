6b843fa8d24488808b6ee5c881ec418cabd5f6bc
blinxduh <111649405+blinxduh@users.noreply.github.com>
2022-10-27 20:27:04 +0100

    Create UtilityHandler.lua

---
+++ UtilityHandler.lua
@@ -0,0 +1,7 @@
+local utils = {
+    ["Animation"] = loadstring(game:HttpGet('https://raw.githubusercontent.com/blinxduh/Roblox-Utils/main/AnimationUtils.lua', true))(),
+    ["Mouse"] = loadstring(game:HttpGet('https://raw.githubusercontent.com/blinxduh/Roblox-Utils/main/MouseUtils.lua', true))(),
+    ["Player"] = loadstring(game:HttpGet('https://raw.githubusercontent.com/blinxduh/Roblox-Utils/main/PlayerUtils.lua', true))()
+}
+
+return utils
